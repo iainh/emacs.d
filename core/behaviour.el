0fddda89e895c87e0c4f568972d44405259bdea5
Iain H <iain@spiralpoint.org>
2021-10-01 22:10:16 -0400

    Start smartparens globally

--- core/behaviour.el
+++ core/behaviour.el
@@ -10,6 +10,10 @@
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
+;; smart parentheses
+(require 'smartparens-config)
+(smartparens-global-mode t)
+
 
 (provide 'behaviour)
 
