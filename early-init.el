1e442ebd1e7853dfd518fa7a18b38397f5b52f6e
Iain H <iain@spiralpoint.org>
2021-10-10 12:13:05 -0400

    Improve frame size comment.
    Indicate there the frame size is being set in case we can't find it in
    the future.

--- early-init.el
+++ early-init.el
@@ -19,10 +19,12 @@
 
 ;; Resizing the Emacs frame can be a terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
-;; larger than the system default.
+;; larger than the system default. We will set the initial frame size to something
+;; larger in core/behaviour.el.
 (setq frame-inhibit-implied-resize t)
 
 ;; Ignore X resources; its settings would be redundant with the other settings
 ;; in this file and can conflict with later config (particularly where the
 ;; cursor color is concerned).
 (advice-add #'x-apply-session-resources :override #'ignore)
+
