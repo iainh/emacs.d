4ba84143c1691b76996ed1dec99a59b228fb421f
Iain H <iain@spiralpoint.org>
2021-10-09 22:35:44 -0400

    Add crux, rework some company config and add modus themes

--- core/behaviour.el
+++ core/behaviour.el
@@ -11,6 +11,14 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
+;; requires that crux be installed
+(global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
+(global-set-key (kbd "C-c o") #'crux-open-with)
+(global-set-key [(shift return)] #'crux-smart-open-line)
+(global-set-key (kbd "s-r") #'crux-recentf-find-file)
+(global-set-key (kbd "C-<backspace>") #'crux-kill-line-backwards)
+(global-set-key [remap kill-whole-line] #'crux-kill-whole-line)
+
 ;; show trailing whitespace
 (setq show-trailing-whitespace t)
 
--- core/packages.el
+++ core/packages.el
@@ -1,4 +1,5 @@
 ;; Install packages using straight.el
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -17,6 +18,8 @@
 (use-package straight
 	     :custom (straight-use-package-by-default t))
 
+(use-package crux)
+
 (use-package exec-path-from-shell)
 (use-package smartparens)
 (use-package markdown-mode)
@@ -26,10 +29,8 @@
 ;;(use-package restclient)	       
 (use-package which-key
   :diminish)
-;;(use-package treemacs)
+
 (use-package lsp-mode)
-;;(use-package lsp-treemacs
-;;  :init (lsp-treemacs-sync-mode 1))
 (use-package yasnippet
   :ensure
   :config
@@ -57,86 +58,66 @@
   (when buffer-file-name
     (setq-local buffer-save-without-query t)))
 
-(use-package projectile)
+;;(use-package projectile)
 (use-package ivy
   :init (ivy-mode t))
 
 (use-package company
   :demand t
-  :hook ((prog-mode-hook . global-company-mode)
-         (text-mode-hook . global-company-mode)
-         (org-mode-hook . global-company-mode)
-         (markdown-mode-hook . global-company-mode))
-  :init
-   (setq company-minimum-prefix-length 3
-        company-require-match 0
-        company-selection-wrap-around t
-        company-tooltip-limit 10
-        company-show-numbers t
-        company-idle-delay 0.05)
-  :bind* (("M-/"	. company-complete)
-          )
-  ;;        ("C-j f"	. company-files)
-  ;;        ("C-j s"	. company-ispell)
-  ;;        ("C-j e"	. company-elisp)
-  ;;        ("C-j y"	. company-yasnippet)
-  ;;        ("C-j c"	. company-dabbrev-code)
-  ;;        ("C-j d"	. company-dabbrev))
-  ;; :bind (:map prog-mode-map
-  ;;      ("C-d" . company-dabbrev-code))
-  ;; :bind (:map text-mode-map
-  ;;      ("C-d" . company-dabbrev))
-  :bind (:map company-active-map
-              ([return] . company-complete-selection))
-  ;;      ("C-n"    . company-select-next)
-  ;;      ("C-p"    . company-select-previous)
-        ([tab]    . yas-expand)
-        ("TAB"    . yas-expand)
-  ;;      ("C-w"    . backward-kill-word)
-        ("C-c"    . company-abort)
-	;;("C-c"    . company-search-abort))
-	:diminish (company-mode . " ς")
-	:config
-	(setq company-backends
-	      '((;; generic backends
-       		 company-files    ; files & directories
-		 company-keywords ; keywords
-		 company-elisp    ;emacs-lisp code
-		 ;;company-shell    ; shell
-		 company-capf     ; completion at point
-;;		 company-lsp      ; LSP
-		 )))
-	 (global-company-mode))
- 
-(use-package flycheck)
-;; (use-package lsp-ui
-;;   :ensure
-;;   :commands lsp-ui-mode
-;;   :custom
-;;   (lsp-ui-peek-always-show t)
-;;   (lsp-ui-sideline-show-hover t)
-;;   (lsp-ui-doc-enable nil))
+  :diminish company-mode
+  :bind (("M-/"  .  company-complete)
+	 :map company-active-map
+	 ("C-p"  .  company-select-previous)
+	 ("C-n"  .  company-select-next)
+	 ("Tab"  .  company-complete-common-or-cycle)
+	 )
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
 
-(use-package spacemacs-theme
-  :defer t
-;;  :init (load-theme 'spacemacs-light t)
-)
-(use-package doom-themes
-  :defer t
-  :init (load-theme 'doom-one-light t)
-  )
+  (setq company-idle-delay 0.05
+	company-minimum-prefix-length 2
+	company-selection t
+	company-require-match nil)
+  (setq company-backends
+ 	'((
+	   company-capf     ; completion at point
+	   company-files    ; files & directories
+ 	   company-keywords ; keywords
+	   company-elisp    ; emacs-lisp code
+	   )))
+   :hook ((prog-mode-hook . global-company-mode)
+ 	 (org-mode-hook . glabal-company-mode)
+ 	 (markdown-mode-hook . global-company-mode))
+ )
+(use-package flycheck)
+
+;;(use-package spacemacs-theme
+;;  :defer t)
+;;(use-package doom-themes
+  ;; :defer t
+  ;; :init (load-theme 'doom-one-light t))	
 (use-package diminish
   :config (diminish 'eldoc-mode))
+(use-package modus-themes
+  :ensure t
+  :init
+  ;; Add all your customizations prior to loading the themes
+  (setq modus-themes-italic-constructs t
+	modus-themes-bold-constructs nil)
+ 
+  ;; Load the theme files before enabling a theme
+  (modus-themes-load-themes)
+  (load-theme 'modus-vivendi t)
+  ;; Swap between light and dark
+  :bind ("<f5>" . modus-themes-toggle)
+  )
+
 (use-package doom-modeline
   :config (doom-modeline-mode)
   (setq doom-modeline-height 1)
   (setq doom-modeline-icon 'nil))
-(use-package buffer-move
-  :config
-  (global-set-key (kbd "<C-S-up>")     'buf-move-up)
-  (global-set-key (kbd "<C-S-down>")   'buf-move-down)
-  (global-set-key (kbd "<C-S-left>")   'buf-move-left)
-  (global-set-key (kbd "<C-S-right>")  'buf-move-right))
 
 (provide 'packages)
 ;;; end of packages
