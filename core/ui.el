6c9c7ae1cc5d0619e0e3a18f75839341bf6cd6cf
Iain H <iain@spiralpoint.org>
2021-10-11 21:53:56 -0400

    Move guard vertical centering hack
    Adjusting the line spacing is only possible in the presence of a
    windowing system and not on the console so don't run our hack on the
    console. This might improve performance but at the very worst we'll
    skip running something unnecessary.

--- core/ui.el
+++ core/ui.el
@@ -22,31 +22,28 @@
                                             (abbreviate-file-name (buffer-file-name))
                                           "%b"))))
 
-;; cursor: Use a red bar cursor rather than the standard block
+;; cursor: Use a bar cursor rather than the standard block
 (setq-default cursor-type 'bar)
-(set-cursor-color "#ff0000")
 
 ;; show line numbers at the beginning of each line
 (if (fboundp 'global-display-line-numbers-mode)
     (global-display-line-numbers-mode)
   (global-nlinum-mode t))
 
-(when window-system
-  ;; Font configuration
-  (set-face-attribute 'default nil :font "Inconsolata" :height 180)
-  (set-face-attribute 'mode-line nil :height 130)
-  (set-face-attribute 'mode-line-inactive nil :height 130))
-
 ;; highlight the current line
 (global-hl-line-mode +1)
 
-;; A hack to vertically centre the text on a line until emacs supports the true centering
-;; Patch by Jesse Medeiros started 2019:
-;;   https://yhetil.org/emacs-devel/87eeewak2c.fsf@gnus.org/T/#u 
-(defun set-bigger-spacing ()
-  (setq-local default-text-properties '(line-spacing 0.125 line-height 1.125)))
-(add-hook 'text-mode-hook 'set-bigger-spacing)
-(add-hook 'prog-mode-hook 'set-bigger-spacing)
+(when window-system
+  ;; Font configuration
+  (set-face-attribute 'default nil :font "Inconsolata" :height 180)
+
+  ;; A hack to vertically centre the text on a line until emacs supports the true centering
+  ;; Patch by Jesse Medeiros started 2019:
+  ;;   https://yhetil.org/emacs-devel/87eeewak2c.fsf@gnus.org/T/#u 
+  (defun set-bigger-spacing ()
+    (setq-local default-text-properties '(line-spacing 0.125 line-height 1.125)))
+  (add-hook 'text-mode-hook 'set-bigger-spacing)
+  (add-hook 'prog-mode-hook 'set-bigger-spacing))
 
 (provide 'ui)
 ;;; end of ui.el
