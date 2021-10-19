03bfa383c2dc74def7e6358f8270d5efb1eaafe6
Iain H <iain@spiralpoint.org>
2021-10-19 09:13:52 -0400

    Add tree-sitter integration on Windows for improved syntax highlighting
    Eventually this can be moved to core/packages.el once we have an arm64 build of tree-sitter for macOS.

--- core/windows.el
+++ core/windows.el
@@ -7,4 +7,14 @@
 (global-unset-key [(control z)])
 (global-unset-key [(control x)(control z)])
 
+;; Tree sitter binaries are available for Windows so configure the
+;; packages here. In the future will move to the common packages.el
+;; when there are arm binaries available or we can automate the build.
+(use-package tree-sitter
+  :init (global-tree-sitter-mode +1)
+  :hook (tree-sitter-after-on . tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :after tree-sitter)
+
 (provide 'windows)
